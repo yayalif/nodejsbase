0803940d15c330f5b61ad87d16ac885f4b2afc63
yayalif <35618582+yayalif@users.noreply.github.com>
2020-04-02 17:36:17 +0800

    Create render.js

---
+++ render.js
@@ -0,0 +1,9 @@
+module.exports = function(template, options) {
+  var str = require('fs').readFileSync(template, 'utf8')
+  // 获取 jade 模板编译处理函数
+  var fn = jade.compile(str, {filename: template, pretty: true})
+  // 调用 fn 函数，将 jade 模板转化为 html 文件数据字符
+  var page = fn(options)
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  res.end(page)
+}
